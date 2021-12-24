8d3eb56c090bab5f564539ce4e3601e8ed4ae8ec
ybnr1130 <muuckk@gmail.com>
2021-12-24 16:48:23 +0900

    Add font color

--- HelloApp/ContentView.swift
+++ HelloApp/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("こんにちは")
-            .padding()
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
     }
 }
 
